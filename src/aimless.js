2676f1f6282e18d7ff3a75b51294e3bcccaed06c
Chris Cavalea <ccavalea@dataminr.com>
2023-05-17 22:16:07 -0400

    add first set of functions + tests, adjust package.json

--- src/aimless.js
+++ src/aimless.js
@@ -1,4 +1,4 @@
-const intRange = (i, j) => {
+const intRange = (i, j, big=false) => {
     const min = Math.ceil(i)
     const max = Math.floor(j)
     return Math.floor(Math.random() * (max - min + 1)) + min
@@ -8,7 +8,54 @@ const floatRange = (min, max) => {
     return Math.random() * (max - min) + min
 }
 
+const weighted = (nums, weights) => {
+    const totalWeight = weights.reduce((sum, weight) => sum + weight, 0)
+    const randomNumber = Math.random() * totalWeight
+
+    let cumulativeWeight = 0
+    let selectedIndex = 0
+
+    for (let i = 0; i < nums.length; i++) {
+        cumulativeWeight += weights[i]
+        if (randomNumber < cumulativeWeight) {
+            selectedIndex = i
+            break
+        }
+    }
+
+    return nums[selectedIndex]
+}
+
+const normalDist = (mean, stdDev) => {
+    // Box-Muller transform
+    let u, v, s
+
+    do {
+        u = (Math.random() * 2) - 1
+        v = (Math.random() * 2) - 1
+        s = u ** 2 + v ** 2
+    } while (s === 0 || s >= 1)
+
+    const t = Math.sqrt((-2 * Math.log(s)) / s)
+    const randomNumber = mean + (stdDev * u * t)
+
+    return randomNumber
+}
+
+const seededFunc = (seed) => {
+    // Park-Miller PRNG
+    let currentSeed = seed % 2147483647
+
+    return function () {
+        currentSeed = (currentSeed * 16807) % 2147483647
+        return (currentSeed - 1) / 2147483646
+    }
+}
+
 export default {
     intRange,
-    floatRange
+    floatRange,
+    weighted,
+    normalDist,
+    seededFunc
 }
--- test/index.js
+++ test/index.js
@@ -4,10 +4,68 @@ const aimless = require('../dist/aimless')
 /* eslint-env mocha */
 
 describe('aimless', () => {
-  describe('intRange', () => {
-    it('should produce a random int in range', () => {
-      expect(aimless.intRange(23, 50)).lessThanOrEqual(50)
-      expect(aimless.intRange(23, 50)).greaterThanOrEqual(23)
-    })
+  it('should produce a random int in range', () => {
+    for (let i = 0; i < 20; i++) {
+      expect(aimless.intRange(23, 50)).lessThanOrEqual(50).and.greaterThanOrEqual(23)
+      expect(aimless.intRange(0, 1)).lessThanOrEqual(1).and.greaterThanOrEqual(0)
+      expect(aimless.intRange(-20, 200)).lessThanOrEqual(200).and.greaterThanOrEqual(-20)
+      expect(aimless.intRange(-100, -10)).lessThanOrEqual(-10).and.greaterThanOrEqual(-100)
+    }
+  })
+  it('should produce a random float in range', () => {
+    for (let i = 0; i < 20; i++) {
+      expect(aimless.floatRange(0.1, 0.2)).lessThanOrEqual(0.2).and.greaterThanOrEqual(0.1)
+      expect(aimless.floatRange(10.6, 20.2)).lessThanOrEqual(20.2).and.greaterThanOrEqual(10.6)
+      expect(aimless.floatRange(-20.11, 201.99)).lessThanOrEqual(201.99).and.greaterThanOrEqual(-20.11)
+      expect(aimless.floatRange(-100.01, -10.39)).lessThanOrEqual(-10.39).and.greaterThanOrEqual(-100.01)
+    }
+  })
+  it('should produce a weighted random integer in the range', () => {
+    for (let i = 0; i < 20; i++) {
+      expect(aimless.weighted(
+        [1, 2, 3, 4, 5, 6],
+        [1, 2, 3, 2, 1]
+      )).to.be.within(1, 6)
+    }
+  })
+  it('should produce a weighted random float in the range', () => {
+    for (let i = 0; i < 20; i++) {
+      expect(aimless.weighted(
+        [1.5, 2.7, 3.2, 4.1, 5.0],
+        [1, 2, 3, 2, 1]
+      )).to.be.within(1.5, 5.0)
+    }
+  })
+  it('should produce a random number that follows a normal distribution', () => {
+    const mean = 0
+    const stdDev = 1
+    const numSamples = 10000
+    const samples = []
+
+    for (let i = 0; i < numSamples; i++) {
+      const randomValue = aimless.normalDist(mean, stdDev);
+      samples.push(randomValue);
+    }
+
+    const sampleMean = samples.reduce((sum, value) => sum + value, 0) / numSamples;
+    const sampleVariance = samples.reduce((sum, value) => sum + ((value - sampleMean) ** 2), 0) / numSamples;
+    const sampleStdDev = Math.sqrt(sampleVariance);
+
+    expect(sampleMean).closeTo(0, .05)
+    expect(sampleStdDev).closeTo(1, .05)
+  })
+  it('should produce a seed function that is predictable', () => {
+    const seedFunc = aimless.seededFunc(1233)
+    const result = []
+
+    for (let i = 0; i < 10; i++) {
+      result.push(seedFunc())
+    }
+
+    const newSeedFunc = aimless.seededFunc(1233)
+
+    for (let i = 0; i < 10; i++) {
+      expect(newSeedFunc()).to.equal(result[i])
+    }
   })
 })
